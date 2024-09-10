dc02166638969962ec7b68abeac23058528b5f74
julya-matias <julyamatias07@gmail.com>
2024-09-10 11:31:10 -0300

    Terminando de colocar os dados do usuario (ainda estatico)

--- api-github/src/pages/RepositoriesPage/Profile/index.jsx
+++ api-github/src/pages/RepositoriesPage/Profile/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-import { Container, Header, Avatar, Login, Name } from './styles'
+import {MdGroup, MdLocationCity, MdWork, MdLink} from 'react-icons/md'
+import { Container, Header, Avatar, Login, Name, Inner, Data } from './styles'
 
 
 const Profile = () =>(
@@ -8,9 +9,25 @@ const Profile = () =>(
   <Container>
     <Header>
       <Avatar src = "https://avatars.githubusercontent.com/u/66370170?v=4"/>
-      <Login>devsamurai</Login>
-      <Name>Dev Samurai</Name>
+      <Login>julya-matias</Login>
+      <Name>Julya Matias</Name>
     </Header>
+    <Inner>
+      <Data>
+        <MdGroup size={20}/>
+        30&nbsp;<i>seguidores</i>&nbsp;&middot;&nbsp;10&nbsp;<i>seguindo</i>
+      </Data>
+      <Data>
+        <MdWork size={20}/> UERJ
+      </Data>
+      <Data>
+        <MdLocationCity size={20}/>Nova Friburgo
+      </Data>
+      <Data>
+      <MdLink size={20}/>
+        <a href='https://github.com/julya-matias'>Meu GitHub</a>
+        </Data>
+    </Inner>
   </Container>
 )
 
