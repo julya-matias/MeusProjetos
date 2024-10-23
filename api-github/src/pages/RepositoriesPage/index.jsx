a3b7e4d9448adf7442ab6e2179008b4a3e51de77
julya-matias <julyamatias07@gmail.com>
2024-10-23 20:23:58 -0300

    Componente filtro parte 1 (erro de eslint)

--- api-github/src/pages/RepositoriesPage/index.jsx
+++ api-github/src/pages/RepositoriesPage/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Container, Sidebar, Main } from './styles'
+
 import Profile from './Profile'
 import Repositories from './Repositories'
 
 import Filter from './Filter'
+// import { Description } from './Repositories/Repository/styles'
 
 
 const RepositoriesPage = () =>{
@@ -18,11 +20,57 @@ const RepositoriesPage = () =>{
     following: 9,
   }
 
+  const repositories = [
+    {
+      name: 'Repo 1',
+      description: 'Descrição',
+      html_url: 'wwww.',
+      languague: 'Java'
+    },
+    {
+      name: 'Repo 2',
+      description: 'Descrição',
+      html_url: 'wwww.',
+      languague: 'JavaScript'
+    },
+    {
+      name: 'Repo 3',
+      description: 'Descrição',
+      html_url: 'wwww.',
+      languague: 'Ruby'
+    },
+    {
+      name: 'Repo 4',
+      description: 'Descrição',
+      html_url: 'wwww.',
+      languague: 'React'
+    },
+    {
+      name: 'Repo 5',
+      description: 'Descrição',
+      html_url: 'wwww.',
+      languague: 'PHP'
+    },  {
+      name: 'Repo 6',
+      description: 'Descrição',
+      html_url: 'wwww.',
+      languague: 'C++'
+    },
+  ]
+
+  const languages = [
+    {name: 'JavaScript', count: 5, color: '#f1c40f'},
+    {name: 'Shell', count: 2, color: '#95a5a6'},
+    {name: 'PHP', count: 1, color: '#3498db'},
+    {name: 'Ruby', count: 1, color: '#ad0606'},
+
+
+  ]
 return(
  <Container>
       <Sidebar>
         <Profile user={user}/>
-        <Filter/>
+        <Filter languages={languages} />
       </Sidebar>
       <Main>
         <Repositories/>
